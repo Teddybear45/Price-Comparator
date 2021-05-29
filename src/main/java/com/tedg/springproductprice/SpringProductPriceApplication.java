0a5ea32621638041f80ef4458fdeffa2955695d6
Ted Guan <tedguan2005@gmail.com>
2021-05-28 19:08:00 -0700

    spring boot init

---
+++ src/main/java/com/tedg/springproductprice/SpringProductPriceApplication.java
@@ -0,0 +1,14 @@
+package com.tedg.springproductprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductPriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductPriceApplication.class, args);
+	}
+
+
+}
